cf2a482e0a97da79c7d19a5a4af4a18dbe0f6116
meetpateltech <113581962+meetpateltech@users.noreply.github.com>
2022-09-15 17:05:24 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://superlative-tulumba-010b96.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Meet Patel',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Meet | Full-Stack Developer`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
